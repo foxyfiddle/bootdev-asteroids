dffc10d47ff3a3b9d1b05f25e15421a4a8a8cb84
jbielawski <foxandthefiddle@comcast.net>
2025-07-14 12:58:45 -0500

    Add pygame window functionallity

--- main.py
+++ main.py
@@ -3,10 +3,22 @@
 # throughout this file
 import pygame
 from constants import *
+
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Initialize the pygame library
+
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
